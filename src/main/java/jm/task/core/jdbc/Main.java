bf047f1f42156825bd0e0169d0086b266e5e30f8
Artyom <moypk@inkognito.com>
2025-05-11 21:18:16 +0300

    izm main

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -7,15 +7,6 @@ import org.hibernate.SessionFactory;
 
 public class Main {
     public static void main(String[] args) {
-//        UserDaoJDBCImpl userDaoJDBCImpl = new UserDaoJDBCImpl();
-//        userDaoJDBCImpl.createUsersTable();
-//        userDaoJDBCImpl.saveUser("Иван", "Иванов", (byte) 18);
-//        userDaoJDBCImpl.saveUser("Петр", "Петров", (byte) 20);
-//        userDaoJDBCImpl.saveUser("Мария", "Мариева", (byte) 22);
-//        userDaoJDBCImpl.saveUser("Кирилл", "Кириллов", (byte) 24);
-//        userDaoJDBCImpl.getAllUsers();
-//        userDaoJDBCImpl.cleanUsersTable();
-//        userDaoJDBCImpl.dropUsersTable();
         UserDaoHibernateImpl userDaoHibernate = new UserDaoHibernateImpl();
         userDaoHibernate.createUsersTable();
         userDaoHibernate.saveUser("Иван", "Иванов", (byte) 18);
